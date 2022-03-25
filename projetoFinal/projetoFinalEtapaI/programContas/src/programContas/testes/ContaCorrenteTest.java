82a44937425cb3e25c0a4ce41492115e7ad31bf6
DiasAlcolumbre <leandro.alcolumbre@gmail.com>
2022-03-24 22:00:53 -0300

    Testes unitários do método deposita() foram adicionados à classe ContaCorrenteTest.

--- projetoFinal/projetoFinalEtapaI/programContas/src/programContas/testes/ContaCorrenteTest.java
+++ projetoFinal/projetoFinalEtapaI/programContas/src/programContas/testes/ContaCorrenteTest.java
@@ -3,18 +3,32 @@ package programContas.testes;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
 import programContas.Conta;
 import programContas.ContaCorrente;
+import programContas.exceptions.NegativoNaoSaldoException;
 import programContas.exceptions.SaldoInsuficienteException;
 
 class ContaCorrenteTest {
-
+	
 	@Test
-	void TestSacarValorNegativoDisparaExceptionSaldoInsuficiente() {
+	void testDepositaComValorNegativoDisparaNegativoNaoException() throws Exception {
+		Conta c = new ContaCorrente(1234, 100.00, "Conta Corrente");
+		
+		assertThrows(NegativoNaoSaldoException.class, () -> c.deposita(-200.00));
+	}
+	
+	@Test
+	void testDepositaCasoDeSucesso() throws Exception {
+		Conta c = new ContaCorrente(1234, 100.00, "Conta Corrente");
+		c.deposita(100.00);
+		assertEquals(200.00, c.getSaldo());
+	}
+	
+	@Test
+	void testSacarValorNegativoDisparaExceptionSaldoInsuficiente() {
 		Conta c = new ContaCorrente(1234, 100.00, "Conta Corrente");
 		
 		assertThrows(SaldoInsuficienteException.class, () -> c.sacar(-200.00));
